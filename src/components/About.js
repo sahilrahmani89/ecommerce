29e538859eb561823c79e32e762ebbf433f36cec
SAHIL RAHMANI <sahilrahmani89@gmail.com>
2021-10-15 23:24:26 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import '../App.css'
+import MobileNav from './MobileNav'
 function About() {
     return (
+        <>
+        <MobileNav/>
         <div className='section-padding container'>
             <div className='row justify-content-center'>
                 <div className='col-lg-4 text-center align-item-center'>
@@ -9,6 +12,7 @@ function About() {
                 </div>
             </div>
         </div>
+        </>
     )
 }
 
