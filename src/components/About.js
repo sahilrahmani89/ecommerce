fb72bb90a0ceef7a9cf53858057f4b85fc6affe4
sahilrahmani89 <sahilrahmani89@gmail.com>
2021-10-26 00:05:49 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import '../App.css'
 import MobileNav from './MobileNav'
-<<<<<<< HEAD
+
 import Benifit from './Benifit'
 import about from '../img/about.jpg'
 import Footer from './Footer'
 // 
-=======
->>>>>>> b9fcd1297fd7ddb63884d6b6a7ea07f84023a934
+
 function About() {
-    return (
-        <>
+    return(
+      <>
         <MobileNav/>
-<<<<<<< HEAD
         <Benifit/>
-        <section className="about-inner section-padding">
+  <section className="about-inner section-padding">
     <div className="container">
       <div className="row">
             <div className="col-lg-6 col-md-6 col-12 in-about-content">
@@ -28,29 +26,19 @@ function About() {
                     </p>
                 <div className="mb-30"></div>
                     <button  className="btn btn-detail">Continue Reading</button>
-=======
-        <div className='section-padding container'>
-            <div className='row justify-content-center'>
-                <div className='col-lg-4 text-center align-item-center'>
-                    <div className='my-5 text-info '>Updating Soon...</div>
->>>>>>> b9fcd1297fd7ddb63884d6b6a7ea07f84023a934
-                </div>
-            
-            </div>
-<<<<<<< HEAD
-         <div className="col-lg-6 col-md-6 col-12">
+                    <div className="col-lg-6 col-md-6 col-12">
            <div className="about-img">
              <img src={about} alt='model'></img>
            </div>
          </div>
+          
       </div>
-    </div>
+      </div>
+      </div>
+      </div>
   </section>
    <Footer/>
-=======
-        </div>
->>>>>>> b9fcd1297fd7ddb63884d6b6a7ea07f84023a934
-        </>
+  </>
     )
 }
 
